8bb8d76778ceb6f604b5ed11ede6e2a3856830f7
Lukeslux <Lucas.brnd@gmail.com>
2017-01-18 12:14:17 +0100

    basic structure.

--- src/plotHeftig.c
+++ src/plotHeftig.c
@@ -1,7 +1,20 @@
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "readWAV.h"
+
+
+float** getHeftigheid(char* audiosource)
+{
+}
 
 int main(int argc, char* argv[])
 {
+	if (argc < 2) 
+	{
+		printf("Usage: %s <audiofile>.wav\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	getHeftigheid(argv[1]);
 	return EXIT_SUCCESS;
 }
