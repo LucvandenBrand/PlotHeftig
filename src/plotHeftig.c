67f69cf20bab2722805e7e62ad7ee71c5382590f
Thijs <knaap@live.com>
2017-01-18 12:44:42 +0100

    it compiles and does something

--- src/plotHeftig.c
+++ src/plotHeftig.c
@@ -1,11 +1,62 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "readWAV.h"
+#define _CRT_SECURE_NO_DEPRECATE
+#include <math.h>
+#include "KissFFT/kiss_fft.h"
+
+#define WIN 512
 
 
 float** getHeftigheid(char* audiosource)
 {
+    char *music_file = "tests/hallelujah.wav";
+    FILE *in;
+    char buf[WIN * 2];
+    int nfft = WIN, i, fx;
+    double intensity = 0;
+    kiss_fft_cfg cfg;
+    kiss_fft_cpx cx_in[WIN];
+    kiss_fft_cpx cx_out[WIN];
+    short *sh;
+
+    cfg = kiss_fft_alloc(nfft, 0, 0, 0);
+    in = fopen(music_file, "r");
+    if (!in) {
+        printf("unable to open file: %s\n", music_file);
+        perror("Error");
+        return NULL;
+    }
+    fx = 0;
+    while (fread(buf, 1, WIN * 2, in)) 
+    {
+        for (i = 0;i<WIN;i++) {
+            sh = (short *)&buf[i * 2];
+            cx_in[i].r = (float) (((double)*sh) / 32768.0);
+            cx_in[i].i = 0.0;
+        }
+
+        kiss_fft(cfg, cx_in, cx_out);
+        //Display the value of a position
+        int position = 511;
+        intensity = sqrt(pow(cx_out[position].r, 2) + pow(cx_out[position].i, 2));
+        printf("%9.4f\n", intensity);
+
+        //Display all values
+        
+        for (i = 0;i<WIN;i++) {
+            //printf("Joe: cx_out[i].r:%f\n", cx_out[i].r);
+            //printf("Joe: cx_out[i].i:%f\n", cx_out[i].i);
+            intensity = sqrt(pow(cx_out[i].r,2) + pow(cx_out[i].i,2));
+            printf("%d - %9.4f\n", i, intensity);
+        }
+        
+
+    }
+    free(cfg);
+    //scanf("%d");
+
+    return NULL;
 }
 
 int main(int argc, char* argv[])
